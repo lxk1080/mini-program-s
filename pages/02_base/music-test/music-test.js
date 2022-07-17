d71ccfa7bcad8d6abe09da5f8f7bb66a0e6445cd
unknown <2664276518@qq.com>
2022-07-17 23:56:50 +0800

    feat: update music src

--- pages/02_base/music-test/music-test.js
+++ pages/02_base/music-test/music-test.js
@@ -26,8 +26,8 @@ Page({
 
   play() {
     // 注意：下面的两个 url 只能是外链，本地文件拿不到资源（小程序就是要小，音频文件太大了）
-    this.data.bam.src = 'https://sharefs.ali.kugou.com/202112131300/c559d5493a78e620838b80280e186eb4/KGTX/CLTX001/2a8e1667ead7aa4f43d82d830f6cc91a.mp3'
-    this.data.bam.coverImgUrl = 'http://imge.kugou.com/stdmusic/150/20201208/20201208155701108376.jpg'
+    this.data.bam.src = 'https://webfs.tx.kugou.com/202207172243/249a1becc006b07ab2fdfab85a625835/KGTX/CLTX001/2a8e1667ead7aa4f43d82d830f6cc91a.mp3'
+    this.data.bam.coverImgUrl = 'https://imgessl.kugou.com/stdmusic/20201208/20201208155701108376.jpg'
     this.data.bam.title = 'Lush Life - Faded'
     this.data.bam.play()
   },
